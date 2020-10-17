eb5e9178576b041fe401b8381d4e23630a5c4eec
Jessica Lally <jml19@ic.ac.uk>
2020-10-17 14:44:03 +0100

    Refactor regexes and pre-processing, add gitignore

--- main.py
+++ main.py
@@ -1,66 +1,69 @@
+import re
+
+import nltk
 import pandas as pd
 from pandas import DataFrame
-import re
-import nltk
 
-def separate_msgs(data_set: DataFrame) -> (DataFrame, DataFrame):
-    ham_txt = pd.DataFrame(columns=['Message'])
-    spam_txt = pd.DataFrame(columns=['Message'])
-
-    for index, column in data_set.iterrows():
-        label = column[0]
-        msg = column[1]
-
-        if label == "ham":
-            ham_txt = ham_txt.append({'Message': msg}, ignore_index=True)
-        elif label == "spam":
-            spam_txt = spam_txt.append({'Message': msg}, ignore_index=True)
-        else:
-            raise ValueError("Invalid label {} on line {}".format(label, index))
-
-    return ham_txt, spam_txt
-
-def normalize(data: DataFrame) -> DataFrame:
-    for index, column in data.iterrows():
-        msg = column[0]
+def pre_process(data: DataFrame) -> DataFrame:
+    for row in data:
+        msg = row.lower()
         msg = replace_symbols(msg)
         msg = remove_punctuation(msg)
         msg = remove_stop_words(msg)
-        msg = msg.lower()
         msg = remove_whitespace(msg)
-        msg = stemmed(msg)
-        data = data.replace(column, msg)
+        msg = stemm(msg)
+        data = data.replace(row, msg)
     return data
 
 def replace_symbols(msg: str) -> str:
-    processed = re.sub(r'(?<=\d) +(?=\d)', '', msg)
-    processed = re.sub(r'[\w\-.]+@([\w\-]+\.)+[\w\-]{2,4}', 'emailaddr', processed)
-    processed = re.sub(r'(http[s]?\S+)|(\w+\.[A-Za-z]{2,4}\S*)', 'httpaddr', processed)
-    processed = re.sub(r'[£$]', 'moneysymb', processed)
-    processed = re.sub(r'\b(\+\d{1,2}\s)?\d?[\-(.]?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b', 'phonenumbr', processed)
-    processed = re.sub(r'\d+(\.\d+)?', 'numbr', processed)
-    return processed
+    # Normalises the input data by generifying phone numbers, email address, etc.
+    regexes = [
+        # Removes spaces between numbers for phone number processing
+        ('(?<=\d) +(?=\d)', ''),
+        # Replaces email addresses
+        ('[\w\-.]+@([\w\-]+\.)+[\w\-]{2,10}', 'emailAddress'),
+        # Replaces domains and urls (matching all possible characters in urls)
+        ('\b(https?:\/\/)?[\/\w\-_\$\.\+!\*\'\(\)\,]+\.(\w{2,10})[\/\w\-_\$\.\+!\*\'\(\)\,]*', 'urlAddress'),
+        # Replaces currency symbols
+        ('[£$€]', 'currencySymbol'),
+        # Replaces phone numbers (inc. country code and hyphens)
+        ('(\+\d{1,3})?(\s\()?\d{3}(\)\s)?\d{7,8}', 'phoneNumber'),
+        # Replaces numbers/decimals
+        ('\b\d+(\.\d+)?\b', 'numberSymbol')
+    ]
+
+    for regex, replacement in regexes:
+        msg = re.sub(regex, replacement, msg)
+
+    return msg
+
 
 def remove_whitespace(msg: str) -> str:
-    processed = re.sub(r'\s+', ' ', msg)
-    processed = re.sub(r'^\s+|\s+?$', '', processed)
+    processed = re.sub('\s+', ' ', msg)
+    processed = re.sub('(^\s+|\s+$)', '', processed)
     return processed
 
 def remove_punctuation(msg: str) -> str:
-    return re.sub(r'[^\w\d\s]', '', msg)
+    return re.sub('[^\w\d\s]', '', msg)
 
 def remove_stop_words(msg: str) -> str:
+    # Removes stop words which do not contribute meaning in English phrases
     stop_words = nltk.corpus.stopwords.words('english')
-    return ' '.join(term for term in msg.split() if term not in set(stop_words))
+    processed = []
 
-def stemmed(msg: str) -> str:
+    for word in msg.split(' '):
+        if word not in set(stop_words):
+            processed.append(word)
+
+    return ' '.join(processed)
+
+def stemm(msg: str) -> str:
+    # Removes suffixes to reduce words to only their stem
     stemmer = nltk.PorterStemmer()
     return ' '.join(stemmer.stem(term) for term in msg.split())
 
 def main():
-    data_set = pd.read_table('data/SMSSpamCollection.txt', header=None)
-    (ham_txt, spam_txt) = separate_msgs(data_set)
-    ham_txt = normalize(ham_txt)
-    spam_txt = normalize(spam_txt)
+    data_set = pd.read_table('data/sms-dataset', header=None)
+    pre_processed = pre_process(data_set[1])
 
 main()
